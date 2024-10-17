b7a884ddb5f6cfebe59776e37174ae39ffa01070
arjun-saseendran <arjunsaseendran@icloud.com>
2024-10-17 20:50:50 +0530

    add style.css

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+let row = document.querySelector(".row");
 fetch("https://fakestoreapi.com/products")
   .then((res) => res.json())
-  .then((json) => console.log(json));
+  .then((data) => {
+    const products = data;
+
+    products.map((product) => {
+      let col = document.createElement("div");
+      col.classList.add("col-12", "col-sm-6", "col-md-4", "col-xl-3");
+
+      col.innerHTML = `
+        <div class="card">
+  <img src=${product.image} class="card-img-top"  style='height: "200px", objectFit: "contain", padding: "10px"'>
+  <div class="card-body">
+    <h5 class="card-title">${product.title}</h5>
+    <p class="card-text">${product.description}</p>
+    <p class="card-text">$${product.price}</p>
+    <a href="#" class="btn btn-primary">Add to cart</a>
+  </div>
+</div>`;
+
+      row.append(col);
+    });
+  });
